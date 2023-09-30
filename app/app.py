cd48a51d21d3256c12681cbf44522d6502cae673
kadenlalani7 <kadenlalani7@users.noreply.github.com>
2023-09-30 15:40:13 -0700

    ok

--- app/app.py
+++ app/app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from prices_api import get_purchase_data
 from config import DevelopmentConfig, ProductionConfig
+import os
 
 app = Flask(__name__)
 
 # Configure the app based on the environment
-if app.debug:
+if os.environ.get("FLASK_ENV") == "development":
     app.config.from_object(DevelopmentConfig)
 else:
     app.config.from_object(ProductionConfig)
